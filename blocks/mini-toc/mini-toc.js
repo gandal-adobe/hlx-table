01909d5f863eabb61f97d269c42c55d52993b7fe
Gali Andal <gandal@adobe.com>
2023-10-16 21:18:12 -0400

    try

---
+++ blocks/mini-toc/mini-toc.js
@@ -0,0 +1,10 @@
+/*
+ * Table Block
+ * Recreate a table
+ * https://www.hlx.live/developer/block-collection/table
+ */
+
+export default async function decorate(block) {
+  const pic = col.querySelector('ul');
+  console.log(pic);
+}
